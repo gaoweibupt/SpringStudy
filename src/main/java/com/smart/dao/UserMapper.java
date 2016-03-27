71aebe785b11fa927d1f698e4d3c80faed3e418c
gaoweibupt <gaoweibupt@163.com>
2016-03-28 00:40:50 +0800

    更新

--- src/main/java/com/smart/dao/UserMapper.java
+++ src/main/java/com/smart/dao/UserMapper.java
@@ -2,12 +2,18 @@ package com.smart.dao;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
 import com.smart.domain.User;
 
 /**
 * @author : gaoweibupt
 * Date:2016年3月27日下午8:34:35
 */
+@Repository
+@Transactional
 public interface UserMapper {
 	public User selectUserByPrimaryKey(String userId);
 	
